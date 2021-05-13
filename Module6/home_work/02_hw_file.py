4f4a39fd472f9121dad47b063fda329b720c0749
ElenLit <83123756+ElenLit@users.noreply.github.com>
2021-05-14 00:35:19 +0300

    Update 02_hw_file.py

--- Module6/home_work/02_hw_file.py
+++ Module6/home_work/02_hw_file.py
@@ -1,5 +1,14 @@
 # Дан файл data/info.txt, в каждой строке которого содержится строка или целое число
 # Найдите сумму всех чисел, пропуская все строки содержащие не числовые значения
 
-with open("data/info.txt", "r") as f:
-    pass
+summa = 0
+with open("info.txt", "r") as f:
+    for line in f:
+        line = line.rstrip('\r\n')
+        try:
+            line = int(line)
+            summa += line
+        except ValueError:
+            pass
+
+print(f'Сумма всех чисел равна {summa}')
